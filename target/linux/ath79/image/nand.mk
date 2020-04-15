dc72542add54067c18bd4b43890684f8c1c9604e
GL.iNet-Li.Zhang <li.zhang@gl-inet.com>
2020-04-15 11:49:22 +0800

    feat: GL-MV1000 imagebuilder 3.2

---
+++ target/linux/ath79/image/nand.mk
@@ -0,0 +1,27 @@
+define Device/glinet_gl-ar300m-nand
+  ATH_SOC := qca9531
+  DEVICE_TITLE := GL-AR300M (NAND)
+  DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-usb-storage kmod-usb-ledtrig-usbport
+  KERNEL_SIZE := 2048k
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  VID_HDR_OFFSET := 512
+  IMAGES += factory.ubi
+  IMAGE/sysupgrade.bin := sysupgrade-tar
+  IMAGE/factory.ubi := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
+endef
+#TARGET_DEVICES += glinet_gl-ar300m-nand
+
+define Device/glinet_gl-e750
+  ATH_SOC := qca9531
+  DEVICE_TITLE := GL-E750
+  DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-usb-storage kmod-usb-ledtrig-usbport
+  KERNEL_SIZE := 2048k
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  VID_HDR_OFFSET := 2048
+  IMAGES += factory.ubi
+  IMAGE/sysupgrade.bin := sysupgrade-tar
+  IMAGE/factory.ubi := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-ubi
+endef
+TARGET_DEVICES += glinet_gl-e750
